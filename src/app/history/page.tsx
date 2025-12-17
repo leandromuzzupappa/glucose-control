fea75a417bc1323cd426e6a0fb1e632b12f00a04
Leandro Muzzupappa <lnmuzzupappa@gmail.com>
2025-12-17 17:55:33 -0300

    refactor: update history page to include the history table component and add more css spacing variables

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -1,38 +1,108 @@
 import { Header } from "@/components/Header";
+import styles from "./page.module.css";
+import { HistoryTable } from "@/components/HistoryTable";
+import { HistoryEntry } from "@/components/HistoryTable/HistoryTable.types";
 
 export default function HistoryPage() {
+  const data: HistoryEntry[] = [
+    {
+      mealType: "breakfast",
+      glucoseLevel: 99,
+      date: "2025-12-15",
+      _createdAt: "2025-12-15T19:24:56+00:00",
+    },
+    {
+      mealType: "dinner",
+      glucoseLevel: 158,
+      date: "2025-12-15",
+      _createdAt: "2025-12-15T17:46:56+00:00",
+    },
+    {
+      mealType: "dinner",
+      glucoseLevel: 33,
+      date: "2025-12-15",
+      _createdAt: "2025-12-15T17:45:16+00:00",
+    },
+    {
+      mealType: "breakfast",
+      glucoseLevel: 120,
+      date: "2025-12-14",
+      _createdAt: "2025-12-14T21:37:20+00:00",
+    },
+    {
+      mealType: "dinner",
+      glucoseLevel: 54,
+      date: "2025-12-14",
+      _createdAt: "2025-12-14T21:34:53+00:00",
+    },
+    {
+      mealType: "lunch",
+      glucoseLevel: 25,
+      date: "2025-12-14",
+      _createdAt: "2025-12-14T21:34:45+00:00",
+    },
+    {
+      mealType: "breakfast",
+      glucoseLevel: 79,
+      date: "2025-12-14",
+      _createdAt: "2025-12-14T21:34:36+00:00",
+    },
+    {
+      mealType: "dinner",
+      glucoseLevel: 79,
+      date: "2025-12-14",
+      _createdAt: "2025-12-14T21:34:15+00:00",
+    },
+    {
+      mealType: "breakfast",
+      glucoseLevel: 11,
+      date: "2025-12-14",
+      _createdAt: "2025-12-14T21:33:55+00:00",
+    },
+    {
+      mealType: "lunch",
+      glucoseLevel: 44,
+      date: "2025-12-12",
+      _createdAt: "2025-12-12T22:23:55+00:00",
+    },
+    {
+      mealType: "dinner",
+      glucoseLevel: 55,
+      date: "2025-12-12",
+      _createdAt: "2025-12-12T06:26:54+00:00",
+    },
+    {
+      mealType: "breakfast",
+      glucoseLevel: 69,
+      date: "2025-12-12",
+      _createdAt: "2025-12-12T06:26:12+00:00",
+    },
+    {
+      mealType: "breakfast",
+      glucoseLevel: 132,
+      date: "2025-12-01",
+      _createdAt: "2025-12-11T19:17:20+00:00",
+    },
+    {
+      mealType: "dinner",
+      glucoseLevel: 321,
+      date: "2025-11-01",
+      _createdAt: "2025-11-01T19:17:20+00:00",
+    },
+    {
+      mealType: "lunch",
+      glucoseLevel: 111,
+      date: "2025-10-18",
+      _createdAt: "2025-10-18T12:17:20+00:00",
+    },
+  ];
+
   return (
     <>
       <Header />
       <main>
-        <h1>My History</h1>
-
-        <table>
-          <thead>
-            <tr>
-              <th>Fecha</th>
-              <th>Comida</th>
-              <th>Glucosa</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>2024-06-01</td>
-              <td>Desayuno</td>
-              <td>110 mg/dL</td>
-            </tr>
-            <tr>
-              <td>2024-06-01</td>
-              <td>Almuerzo</td>
-              <td>130 mg/dL</td>
-            </tr>
-            <tr>
-              <td>2024-06-01</td>
-              <td>Cena</td>
-              <td>120 mg/dL</td>
-            </tr>
-          </tbody>
-        </table>
+        <h1>Historial</h1>
+        <HistoryTable className={styles.historyTable} data={data} />
       </main>
     </>
   );
