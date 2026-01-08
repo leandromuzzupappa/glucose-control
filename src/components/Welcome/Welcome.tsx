f69152cdeab8e9a66c5728497597d8c62420a2d9
LMuzzupappa <lnmuzzupappa@gmail.com>
2026-01-08 02:54:51 -0300

    refactor: update welcome screen layout

--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -2,12 +2,11 @@ import styles from "./Welcome.module.css";
 import classNames from "classnames";
 import { ReactElement } from "react";
 import { WelcomeProps } from "./Welcome.types";
-import { useWelcome } from "./Welcome.hooks";
 import { getDateTime } from "./Welcome.utils";
+import { MomentButtonLink } from "../MomentButtonLink";
 import Link from "next/link";
 
 export const Welcome = ({ className }: WelcomeProps): ReactElement => {
-  const { foo } = useWelcome();
   const { date, time } = getDateTime();
 
   return (
@@ -18,17 +17,24 @@ export const Welcome = ({ className }: WelcomeProps): ReactElement => {
         Hoy es <span>{date}</span> y son las <span>{time}</span> horas.
       </p>
 
-      <h2 className={styles.subtitle}>¿Qué queres hacer hoy?</h2>
+      <h2 className={styles.subtitle}>¿Qué queres medir hoy?</h2>
 
-      <ul className={styles.optionsList}>
+      <ul className={styles.momentList}>
         <li>
-          <Link href="new" className={styles.option}>
-            Registrar una nueva medición
-          </Link>
+          <MomentButtonLink href="/new/breakfast" moment="breakfast" />
         </li>
         <li>
+          <MomentButtonLink href="/new/lunch" moment="lunch" />
+        </li>
+        <li>
+          <MomentButtonLink href="/new/dinner" moment="dinner" />
+        </li>
+
+        <hr />
+
+        <li>
           <Link href="history" className={styles.option}>
-            Revisar todos mis niveles de glucosa
+            Ver todo mi historial
           </Link>
         </li>
       </ul>
